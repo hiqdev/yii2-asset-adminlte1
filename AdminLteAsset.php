ae19bdf23c1291e3e7d363aa161d5daaac262deb
Andrii Vasyliev <sol@hiqdev.com>
2015-05-25 16:29:35 +0000

    returned back asset file

---
+++ AdminLteAsset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link        http://hiqdev.com/yii2-asset-adminlte1
+ * @license     BSD 3-clause
+ * @copyright   Copyright (c) 2015 HiQDev
+ */
+
+namespace hipanel\assets\adminlte1;
+
+use yii\web\AssetBundle;
+
+class AdminLteAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/hiqdev/yii2-asset-adminlte1/assets';
+
+    public $css = [
+        'css/morris/morris.css',
+        'css/AdminLTE.css',
+    ];
+
+    public $js = [
+        'js/AdminLTE/app.js',
+    ];
+
+}
